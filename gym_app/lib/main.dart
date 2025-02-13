c48d5166b4a61a1d3822d549e29c7932a4b745bb
xegenie <dltpwlswkdf@naver.com>
2025-02-13 17:06:32 +0900

    결제 중

--- gym_app/lib/main.dart
+++ gym_app/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:gym_app/screens/join.dart';
 import 'package:gym_app/screens/login.dart';
 import 'package:gym_app/screens/my_page.dart';
 import 'package:gym_app/screens/my_page_info.dart';
+import 'package:gym_app/screens/pay_screen.dart';
 import 'package:gym_app/screens/ptList_screen.dart';
 import 'package:gym_app/screens/ptTicket_screen.dart';
 import 'package:gym_app/screens/reservation_insert_screen.dart';
@@ -41,8 +42,9 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
+      initialRoute: '/',
       routes: {
+        '/': (context) => PayScreen(),
         '/home': (context) => HomeContent(),
         '/ticket': (context) => TicketScreen(),
         '/trainer': (context) => TrainerScreen(),
