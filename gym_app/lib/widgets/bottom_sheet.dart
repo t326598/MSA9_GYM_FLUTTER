d1fe5ae6fdc04f1a9ef44daeb65175a744bf32c3
오똥땅 <ajtwodtmdnjs@gmail.com>
2025-02-13 12:42:15 +0900

    bottom sheet 통일

--- gym_app/lib/widgets/bottom_sheet.dart
+++ gym_app/lib/widgets/bottom_sheet.dart
@@ -14,33 +14,34 @@ class CustomBottomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SalomonBottomBar(
+      backgroundColor: Color.fromARGB(100, 100, 100, 100),
       currentIndex: currentIndex,
       onTap: (index) {
         onTap(index); // 선택한 아이템의 인덱스를 설정
       },
       items: [
         SalomonBottomBarItem(
-          icon: const Icon(Icons.home, color:Colors.black),
+          icon: const Icon(Icons.home, color:Colors.white),
           title: const Text("홈"),
           selectedColor: Colors.blue,
         ),
         SalomonBottomBarItem(
-          icon: const Icon(Icons.confirmation_number_outlined, color: Colors.black),
+          icon: const Icon(Icons.confirmation_number_outlined, color: Colors.white),
           title: const Text("티켓"),
           selectedColor: Colors.pink,
         ),
         SalomonBottomBarItem(
-          icon: const Icon(Icons.event_available , color: Colors.black),
+          icon: const Icon(Icons.event_available , color: Colors.white),
           title: const Text("예약"),
           selectedColor: Colors.green,
         ),
         SalomonBottomBarItem(
-          icon: const Icon(Icons.calendar_today_sharp , color: Colors.black),
+          icon: const Icon(Icons.calendar_today_sharp , color: Colors.white),
           title: const Text("캘린더"),
           selectedColor: Colors.purple,
         ),
         SalomonBottomBarItem(
-          icon: const Icon(Icons.list , color: Colors.black),
+          icon: const Icon(Icons.list , color: Colors.white),
           title: const Text("메뉴"),
           selectedColor: Colors.teal,
         ),
