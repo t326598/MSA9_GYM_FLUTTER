ad141dcbf65e1c74d9aa6e8df7f939d15c2b26fd
오똥땅 <ajtwodtmdnjs@gmail.com>
2025-02-14 12:54:44 +0900

    헬스장 입장 시 로그인 유무 확인

--- gym_app/lib/screens/home_screen.dart
+++ gym_app/lib/screens/home_screen.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:gym_app/service/home_service.dart';
 import 'package:uuid/uuid.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-
+import 'package:provider/provider.dart';
 import '../widgets/bottom_sheet.dart';
+import '../provider/user_provider.dart'; // ✅ 추가
 
 class HomeContent extends StatefulWidget {
   const HomeContent({super.key});
@@ -43,7 +44,7 @@ class _HomeContentState extends State<HomeContent> {
       return count;
     } catch (e) {
       print('Error fetching user count: $e');
-      return 0; // 에러 시 0명으로 반환
+      return 0;
     }
   }
 
@@ -114,175 +115,190 @@ class _HomeContentState extends State<HomeContent> {
     });
   }
 
+  // SnackBar를 반환하는 함수
+  SnackBar customSnackbar() {
+    return const SnackBar(
+      content: Row(
+        children: [
+          Icon(Icons.check_circle, color: Colors.white),
+          SizedBox(width: 8),
+          Text("로그인이 필요한 기능입니다."),
+        ],
+      ),
+      backgroundColor: Colors.red,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Theme(
-        data: ThemeData(
-          scaffoldBackgroundColor: Color.fromARGB(255, 49, 47, 47),
-          colorScheme: ColorScheme.dark(
-            primary: const Color.fromARGB(255, 206, 61, 61),
-            onPrimary: const Color.fromARGB(255, 241, 239, 239),
-            surface: const Color.fromARGB(255, 178, 10, 201),
-            onSurface: Color.fromARGB(255, 9, 191, 211),
-          ),
+      data: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 49, 47, 47),
+        colorScheme: const ColorScheme.dark(
+          primary: Color.fromARGB(255, 206, 61, 61),
+          onPrimary: Color.fromARGB(255, 241, 239, 239),
+          surface: Color.fromARGB(255, 178, 10, 201),
+          onSurface: Color.fromARGB(255, 9, 191, 211),
         ),
-        child: Scaffold(
-          body: Stack(
-            fit: StackFit.expand,
-            children: [
-              ColorFiltered(
-                colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.5),
-                  BlendMode.darken,
-                ),
-                child: Image.asset(
-                  'images/main.png',
-                  fit: BoxFit.cover,
-                ),
+      ),
+      child: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.5),
+                BlendMode.darken,
               ),
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 0),
-                    const Text(
-                      '실시간 헬스장 이용자 수',
-                      style: TextStyle(
-                        fontSize: 23,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        height: 0,
-                      ),
+              child: Image.asset(
+                'images/main.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    '실시간 헬스장 이용자 수',
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
-                    const SizedBox(height: 10),
-                    FutureBuilder<int>(
-                      future: fetchGymUserCount(),
-                      builder: (context, snapshot) {
+                  ),
+                  const SizedBox(height: 10),
+                  FutureBuilder<int>(
+                    future: fetchGymUserCount(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else {
+                        int gymUserCount = snapshot.data ?? 0;
                         String statusMessage = '';
                         Color statusColor = Colors.transparent;
 
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return CircularProgressIndicator();
-                        } else if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        } else if (snapshot.hasData) {
-                          int gymUserCount = snapshot.data!;
+                        if (gymUserCount < 10) {
+                          statusMessage = '여유';
+                          statusColor = Colors.green;
+                        } else if (gymUserCount < 20) {
+                          statusMessage = '혼잡';
+                          statusColor = Colors.yellow;
+                        } else {
+                          statusMessage = '포화';
+                          statusColor = Colors.red;
+                        }
 
-                          if (gymUserCount < 10) {
-                            statusMessage = '여유';
-                            statusColor = Colors.green;
-                          } else if (gymUserCount >= 10 && gymUserCount < 20) {
-                            statusMessage = '혼잡';
-                            statusColor = Colors.yellow;
-                          } else {
-                            statusMessage = '포화';
-                            statusColor = Colors.red;
-                          }
-
-                          return Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '$gymUserCount명  /',
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '$gymUserCount명  /',
+                              style: const TextStyle(
+                                fontSize: 23,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 8, vertical: 4),
+                              decoration: BoxDecoration(
+                                color: statusColor,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text(
+                                statusMessage,
                                 style: const TextStyle(
                                   fontSize: 23,
                                   fontWeight: FontWeight.bold,
                                   color: Colors.white,
                                 ),
                               ),
-                              const SizedBox(width: 10),
-                              Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 8, vertical: 4),
-                                decoration: BoxDecoration(
-                                  color: statusColor,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Text(
-                                  statusMessage,
-                                  style: const TextStyle(
-                                    fontSize: 23,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          );
+                            ),
+                          ],
+                        );
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  Image.asset(
+                    'images/logo.png',
+                    height: 100,
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(80.0),
+                child: Consumer<UserProvider>(
+                  builder: (context, userProvider, child) {
+                    return ElevatedButton(
+                      onPressed: () {
+                        if (userProvider.isLogin) {
+                          _showQRCodeModal(context);
                         } else {
-                          return Text('No data available');
+                          // SnackBar 알림을 띄우고 로그인 화면으로 이동
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(customSnackbar());
+                          Future.delayed(const Duration(seconds: 2), () {
+                            Navigator.pushNamed(context, '/login');
+                          });
                         }
                       },
-                    ),
-                    const SizedBox(height: 20),
-                    Image.asset(
-                      'images/logo.png',
-                      height: 100,
-                    ),
-                    const SizedBox(height: 10),
-                  ],
-                ),
-              ),
-              Positioned(
-                top: 50,
-                left: 125,
-                child: Image.asset(
-                  'images/icon.png',
-                  height: 190,
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.all(80.0),
-                  child: ElevatedButton(
-                    onPressed: () => _showQRCodeModal(context),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 88, 72, 90),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 40, vertical: 15),
-                      textStyle: const TextStyle(fontSize: 18),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 88, 72, 90),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 15),
+                        textStyle: const TextStyle(fontSize: 18),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        elevation: 10,
                       ),
-                      elevation: 10,
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: const [
-                        Icon(
-                          Icons.qr_code,
-                          color: Colors.white,
-                          size: 24,
-                        ),
-                        SizedBox(width: 8),
-                        Text(
-                          '헬스장 입장',
-                          style: TextStyle(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Icon(
+                            Icons.qr_code,
                             color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                            size: 24,
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
+                          SizedBox(width: 8),
+                          Text(
+                            '헬스장 입장',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
-            ],
-          ),
-          bottomNavigationBar: CustomBottomNavigationBar(
-            currentIndex: _currentIndex,
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
+            ),
+          ],
+        ),
+        bottomNavigationBar: CustomBottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
 
-              if (index >= 0 && index < routes.length) {
-                Navigator.pushReplacementNamed(context, routes[index]);
-              }
-            },
-          ),
-        ));
+            if (index >= 0 && index < routes.length) {
+              Navigator.pushReplacementNamed(context, routes[index]);
+            }
+          },
+        ),
+      ),
+    );
   }
 }
